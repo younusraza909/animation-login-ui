6105290a84dcc737de074097ce3e9cd612c13a7a
younusraza909 <younusraza909@gmail.com>
2024-05-11 14:41:28 +0500

    Sliding animation

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const button = document.getElementById('cta-outline')
-const signinWrapper = document.getElementById('signin-form-wrapper')
-const signupCTA = document.getElementById("signup-cta-wrapper")
+const buttons = document.getElementsByClassName('cta-outline')
+const signinWrapper = document.getElementById('form-wrapper')
+const signupCTA = document.getElementById("cta-wrapper")
 
-button.addEventListener('click', () => {
-    signinWrapper.classList.toggle('toggle')
-    signupCTA.classList.toggle('toggle')
-})
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i]
+    button.addEventListener('click', () => {
+        signinWrapper.classList.toggle('toggle')
+        signupCTA.classList.toggle('toggle')
+    })
+}
